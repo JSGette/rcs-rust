1c4ea34499b6319daa5798dafe21c9a3648db0d6
Joseph Gette <jgettepost@gmail.com>
2022-12-17 17:09:15 +0100

    Some experiments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+struct Example {
+    boolean_var: bool,
+    unsigned_var: u32,
+    heap_type: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("HELLO, WORLD!");
+    let example = Example {
+        boolean_var: true,
+        unsigned_var: 12,
+        heap_type: String::from("test"),
+    };
+
+    read_example(&example);
+
+    println!("{}", example.boolean_var);
+
+    let example2 = Example{
+        boolean_var: false,
+        ..example
+    };
+    println!("{}", example2.unsigned_var);
+    println!("{}", example.unsigned_var);
+    println!("{}", example2.heap_type);
+}
+
+fn read_example(example: &Example) {
+    println!("Bool: {}", example.boolean_var);
+    println!("Number: {}", example.unsigned_var);
 }
